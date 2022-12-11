e04343182e87d46dede2c1dd915a902a2b0cacba
Viktor Georgakiev <ciganskigeigerwiksa26g@gmail.com>
2022-12-11 13:57:01 +0200

    Login, Register, Navbar, Layout Update

--- TopAutoSpot/Data/Entities/User.cs
+++ TopAutoSpot/Data/Entities/User.cs
@@ -5,8 +5,8 @@ namespace TopAutoSpot.Data.Entities
     public class User : IdentityUser
     {
         // Id, Email, Password, Username and PhoneNumber come from IdentityUser
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
         public string Role { get; set; }
         public List<Listing> Listings { get; set; }
     }
