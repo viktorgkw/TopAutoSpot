c5cb5fc711e3d620a03a8b53ddc20bae14013859
Viktor Georgakiev <ciganskigeigerwiksa26g@gmail.com>
2022-12-14 19:57:16 +0200

    Footer Design fixed and Manage Account Menu too.

---
+++ TopAutoSpot/Views/PersonalData/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TopAutoSpot.Views.PersonalData
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
