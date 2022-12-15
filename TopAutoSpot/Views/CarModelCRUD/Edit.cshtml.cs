2234104b2c99cc7ef94c5d2d3bb9f439ce18af94
viktorgkw <ciganskigeigerwiksa26g@gmail.com>
2022-12-15 17:40:08 +0200

    Some UI Updates. WIP

---
+++ TopAutoSpot/Views/CarModelCRUD/Edit.cshtml.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using TopAutoSpot.Data.Entities;
+
+namespace TopAutoSpot.Views.CarModelCRUD
+{
+    public class EditModel : PageModel
+    {
+        private readonly TopAutoSpot.Data.ApplicationDbContext _context;
+
+        public EditModel(TopAutoSpot.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Car Car { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(string id)
+        {
+            if (id == null || _context.Cars == null)
+            {
+                return NotFound();
+            }
+
+            var car =  await _context.Cars.FirstOrDefaultAsync(m => m.Id == id);
+            if (car == null)
+            {
+                return NotFound();
+            }
+            Car = car;
+            return Page();
+        }
+
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(Car).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CarExists(Car.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool CarExists(string id)
+        {
+          return (_context.Cars?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ TopAutoSpot/Views/CarModelCRUD/Index.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using TopAutoSpot.Data;
+using TopAutoSpot.Data.Entities;
+
+namespace TopAutoSpot.Views.CarModelCRUD
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ApplicationDbContext _context;
+
+        public IndexModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Car> Car { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Cars != null)
+            {
+                Car = await _context.Cars.ToListAsync();
+            }
+        }
+    }
+}
