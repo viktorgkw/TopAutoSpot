7c909082b8232661797d902f8964778c810b8fdd
viktorgkw <ciganskigeigerwiksa26g@gmail.com>
2022-12-17 19:24:24 +0200

    Latest table version. Authorizated all pages as they should be!

--- TopAutoSpot/Views/Auction/Index.cshtml.cs
+++ TopAutoSpot/Views/Auction/Index.cshtml.cs
@@ -1,8 +1,9 @@
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace TopAutoSpot.Views.Auction
 {
+    [Authorize]
     public class IndexModel : PageModel
     {
         public void OnGet()
