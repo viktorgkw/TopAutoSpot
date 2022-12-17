dabe9e67118f3e3596f04aed135b1274a1bb2488
viktorgkw <ciganskigeigerwiksa26g@gmail.com>
2022-12-17 19:41:56 +0200

    From MVC to full Razor Page app.

---
+++ TopAutoSpot/Views/Index.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TopAutoSpot.Views
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
