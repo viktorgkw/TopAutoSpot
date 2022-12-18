e6118f2b677e10f6b16d31e06f19d62844b2bc54
viktorgkw <ciganskigeigerwiksa26g@gmail.com>
2022-12-18 22:00:52 +0200

    Notifications latest stable version.

---
+++ TopAutoSpot/Data/Migrations/20221218163337_NotificationsTableColumnAdded.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TopAutoSpot.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class NotificationsTableColumnAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "To",
+                table: "Notifications",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "To",
+                table: "Notifications");
+        }
+    }
+}
--- TopAutoSpot/Views/Notifications/Index.cshtml.cs
+++ TopAutoSpot/Views/Notifications/Index.cshtml.cs
@@ -1,12 +1,33 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using TopAutoSpot.Data;
+using TopAutoSpot.Data.Entities;
 
 namespace TopAutoSpot.Views.Notifications
 {
+    [Authorize]
     public class IndexModel : PageModel
     {
-        public void OnGet()
+        private ApplicationDbContext _context;
+        public IndexModel(ApplicationDbContext context)
         {
+            _context = context;
+        }
+
+        [BindProperty]
+        public List<Notification> Notifications { get; set; }
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            var currentUser = await _context.Users.FirstAsync(u => u.UserName == User.Identity.Name);
+
+            Notifications = await _context.Notifications
+                .Where(n => n.To == currentUser.Id)
+                .ToListAsync();
+
+            return Page();
         }
     }
 }
