20db9f471b4aaca26568d4a677e63c1d5d328f8c
viktorgkw <ciganskigeigerwiksa26g@gmail.com>
2022-12-18 18:29:07 +0200

    Notification Table added. Notification Page added.

--- TopAutoSpot/Data/Entities/User.cs
+++ TopAutoSpot/Data/Entities/User.cs
@@ -8,5 +8,6 @@ namespace TopAutoSpot.Data.Entities
         public string? FirstName { get; set; }
         public string? LastName { get; set; }
         public string Role { get; set; }
+        public List<Notification>? Notifications { get; set; } = new List<Notification>();
     }
 }
---
+++ TopAutoSpot/Data/Migrations/20221218162827_NotificationsTableAdded.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TopAutoSpot.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class NotificationsTableAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Notification",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    From = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UserId = table.Column<string>(type: "nvarchar(450)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Notification", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Notification_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Notification_UserId",
+                table: "Notification",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Notification");
+        }
+    }
+}
