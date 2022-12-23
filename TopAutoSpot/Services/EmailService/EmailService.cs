8f7363b034a1df7049829bbbc9d6bd300ed2a916
viktorgkw <ciganskigeigerwiksa26g@gmail.com>
2022-12-23 13:25:38 +0200

    NewsAPI now returns different News for each page and in single page there cannot be two same news.

--- TopAutoSpot/Services/EmailService/EmailService.cs
+++ TopAutoSpot/Services/EmailService/EmailService.cs
@@ -1,7 +1,7 @@
-﻿using MailKit.Net.Smtp;
-using MailKit.Security;
-using MimeKit;
+﻿using MimeKit;
 using MimeKit.Text;
+using MailKit.Net.Smtp;
+using MailKit.Security;
 using TopAutoSpot.Models;
 
 namespace TopAutoSpot.Services.EmailService
@@ -18,17 +18,16 @@ namespace TopAutoSpot.Services.EmailService
         public void SendEmail(EmailDto request)
         {
             var email = new MimeMessage();
-            email.From.Add(MailboxAddress.Parse(_configuration.GetSection("EmailUsername").Value));
+            email.From.Add(MailboxAddress.Parse(_configuration["Email:Username"]));
             email.To.Add(MailboxAddress.Parse(request.To));
             email.Subject = request.Subject;
             email.Body = new TextPart(TextFormat.Html) { Text = request.Body };
 
             using var smtp = new SmtpClient();
-            smtp.Connect(_configuration.GetSection("EmailHost").Value,
+            smtp.Connect(_configuration["Email:Host"],
                 587,
                 SecureSocketOptions.StartTls);
-            smtp.Authenticate(_configuration.GetSection("EmailUsername").Value,
-                _configuration.GetSection("EmailPassword").Value);
+            smtp.Authenticate(_configuration["Email:Username"], _configuration["Email:Password"]);
             smtp.Send(email);
             smtp.Disconnect(true);
         }
--- TopAutoSpot/Views/AuctionViews/Join.cshtml.cs
+++ TopAutoSpot/Views/AuctionViews/Join.cshtml.cs
@@ -1,8 +1,11 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace TopAutoSpot.Views.AuctionViews
 {
+    [Authorize]
     public class JoinModel : PageModel
     {
+        // Validate that the auction is not in Status != Active
     }
 }
