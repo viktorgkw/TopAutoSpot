5fcf5407895c1db95c8a8d2469695d12f02915e3
viktorgkw <ciganskigeigerwiksa26g@gmail.com>
2022-12-23 21:22:37 +0200

    Premium accounts added. Stripe working.

---
+++ TopAutoSpot/Services/PaymentServices/IPaymentService.cs
@@ -0,0 +1,9 @@
+﻿using TopAutoSpot.Models;
+
+namespace TopAutoSpot.Services.PaymentServices
+{
+    public interface IPaymentService
+    {
+        Task<string> MakePayment(StripePayment paymentInfo);
+    }
+}
